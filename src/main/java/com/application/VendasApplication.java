288eb436e6d6f87e0cbf57c584437aba0ee54aec
Jefferson Paulino <jpnr2013@gmail.com>
2020-05-10 17:22:24 -0300

    criado as entidades e o dao para acessar o banco de dados

--- src/main/java/com/application/VendasApplication.java
+++ src/main/java/com/application/VendasApplication.java
@@ -1,23 +1,35 @@
 package com.application;
 
+import com.application.domain.dao.ClienteDao;
+import com.application.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
     @Autowired
-    @Qualifier("applicationName")
-    private String applicationName;
+    private ClienteDao clienteDao;
 
-    @GetMapping("/hello")
-    public String helloWorkd() {
-        return applicationName;
+    @Bean
+    public CommandLineRunner init() {
+        return args -> {
+            clienteDao.salvar(new Cliente("Jefferson"));
+            clienteDao.salvar(new Cliente("Outro Jefferson"));
+
+            List<Cliente> clientes = clienteDao.obterTodos();
+
+            clientes.forEach(System.out::println);
+        };
     }
 
     public static void main(String[] args) {
